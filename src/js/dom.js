7b87c878a583de28fb40689cf1a4e5f0e566e97c
Takoulinos <theo100theo@yahoo.gr>
2022-12-18 21:51:08 +0200

    addes doma bootstrap features in addProject

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,23 +1,30 @@
+import { format } from 'date-fns'
+
 export function addProject(project) {
     //add project
     const container = document.querySelector('.container');
     const newProject = document.createElement('div');
+    newProject.classList.add('row')
     container.appendChild(newProject);
     //title
-    const title = document.createElement('h3');
+    const title = document.createElement('div');
     title.textContent = project.title;
+    title.classList.add('col-md');
     newProject.appendChild(title);
     //description
     const description = document.createElement('div');
     description.textContent = project.description;
+    description.classList.add('col-sm')
     newProject.appendChild(description);
     //due date
     const due = document.createElement('div');
-    due.textContent = project.dueDate;
+    due.textContent = format(project.dueDate, 'dd/MM/yyyy');
+    due.classList.add('col-sm');
     newProject.appendChild(due);
     //priority
     const priority = document.createElement('div');
     priority.textContent = project.priority;
+    priority.classList.add('col-sm');
     newProject.appendChild(priority);
     //tasks
     const tasks = document.createElement('div');
--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,5 @@ import * as bootstrap from 'bootstrap'
 import { Project } from './todos.js'
 import { addProject } from './dom';
 
-const def = new Project('default', 'asdf', '2023', 'relaxed', [1,2,3]);
+const def = new Project('default', 'asdf', new Date(), 'relaxed', [1,2,3]);
 addProject(def)
